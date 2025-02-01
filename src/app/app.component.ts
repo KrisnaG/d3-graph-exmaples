28b56061cbe125537bf5be11c5a974c8e01df0cc
Krisna Gusti <kgusti@myune.edu.au>
2025-02-01 20:07:50 +1030

    Added primeng and implemented basic dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { ForceDirectedGraphComponent } from './force-directed-graph/force-directed-graph.component';
+import { Sidebar } from 'primeng/sidebar';
+import { Button } from 'primeng/button';
+import { Menu } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ForceDirectedGraphComponent],
+  imports: [
+    ForceDirectedGraphComponent,
+    Sidebar,
+    Button,
+    Menu,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'd3-graph-examples';
+  sidebarVisible = false;
+  showGraph = false;
+  menuItems: MenuItem[] = [
+    {
+      label: 'Graphs',
+      items: [
+        {
+          label: 'Force Directed Graph',
+          icon: 'pi pi-chart-network',
+          command: () => {
+            this.showGraph = true;
+            this.sidebarVisible = false;
+          }
+        }
+      ]
+    }
+  ];
 }
